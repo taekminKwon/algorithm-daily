ad81ae0960e1d3a36a45239a7dcea5efb70a348e
taekminKwon <rnjsxorals1@gmail.com>
2025-11-26 12:08:15 +0900

    [Silver II] Title: N과 M (9), Time: 156 ms, Memory: 19776 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] arr, arrM;
+    static int N, M;
+    static StringBuilder sb;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        arrM = new int[M];
+        visited = new boolean[N];
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if(depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arrM[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        arrM[depth] = 0;
+        for (int i = 0; i < N; i++) {
+            if(!visited[i] && arrM[depth] != arr[i]) {
+                visited[i] = true;
+                arrM[depth] = arr[i];
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
