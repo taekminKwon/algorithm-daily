d578ec47585a2ea0ecb3b7fc9b90015460c4b6c1
taekminKwon <rnjsxorals1@gmail.com>
2025-12-11 14:23:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.91 ms, Memory: 97.3 MB -BaekjoonHub

--- Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,25 +4,16 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
         String answer = "";
-        for (String name : participant) {
+        for (String name : completion) {
             map.put(name, map.getOrDefault(name, 0) + 1);
         }
         
-        for (String name : completion) {
-            int count = map.get(name) - 1;
-            if (count == 0) {
-                map.remove(name);
-                continue;
-            }
-            
-            map.put(name, count);
-        }
-        
         for (String name : participant) {
-            if (map.containsKey(name)) {
+            int count = map.getOrDefault(name, 0) - 1;
+            if (count < 0) {
                 answer = name;
-                break;
             }
+            map.put(name, count);
         }
     
         return answer;
