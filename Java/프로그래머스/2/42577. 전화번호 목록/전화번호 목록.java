9831908d65c496be47902c0e47470296ed8b88a7
taekminKwon <rnjsxorals1@gmail.com>
2026-09-05 16:50:34 +0900

    [level 2] Title: 전화번호 목록, Time: 222.92 ms, Memory: 118 MB -BaekjoonHub

--- Java/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ Java/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,13 +2,11 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>(Arrays.asList(phone_book));
-        
-        for (String phoneNumber : phone_book) {
-            for (int i = 1; i < phoneNumber.length(); i++) {
-                if (set.contains(phoneNumber.substring(0, i))) {
-                    return false;
-                }
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
             }
         }
         return true;
