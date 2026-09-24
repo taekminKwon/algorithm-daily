0a5b67c5d7972a9dbd0c423f6572ccaf617116ba
taekminKwon <rnjsxorals1@gmail.com>
2026-09-24 14:33:49 +0900

    [level 3] Title: 네트워크, Time: 0.74 ms, Memory: 73.7 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,48 +1,45 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visited;
-    static List<Integer>[] list;
+    static boolean[] isConnected;
     public int solution(int n, int[][] computers) {
-        list = new List[n];
-        visited = new boolean[n];
         int answer = 0;
-        for (int i = 0; i < n; i++) {
-            list[i] = new ArrayList<>();
-            int[] computer = computers[i];
-            
-            for(int j = 0; j < n; j++) {
-                if (i != j && computer[j] == 1) {
-                    list[i].add(j);
+        
+        isConnected = new boolean[computers.length];
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer>[] link = new List[computers.length];
+        for (int i = 0; i < link.length; i++) {
+            link[i] = new ArrayList<>();
+        }
+        
+        for (int i = 0; i < computers.length; i++) {
+            for (int j = i + 1; j < computers[i].length; j++) {
+                if (computers[i][j] == 1) {
+                    link[i].add(j);
+                    link[j].add(i);
                 }
             }
         }
         
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                bfs(i);
+        for (int i = 0; i < computers.length; i++) {
+            if (!isConnected[i]) {
                 answer++;
+                queue.add(i);
+                while (!queue.isEmpty()) {
+                    int next = queue.poll();
+                    for (int j = 0; j < link[next].size(); j++) {
+                        int neighbor = link[next].get(j);
+                        if (isConnected[neighbor]){
+                            continue;
+                        }
+                    
+                        isConnected[neighbor] = true;
+                        queue.add(neighbor);
+                    }
+                }
             }
         }
         
         return answer;
-    }
-    
-    static void bfs(int i) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(i);
-        visited[i] = true;
-        
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-            for (Integer n : list[curr]) {
-                if (visited[n]) {
-                    continue;
-                }
-                
-                visited[n] = true;
-                queue.add(n);
-            }
-        }
     }
 }
